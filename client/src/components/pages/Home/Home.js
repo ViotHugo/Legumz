3e49396f7fd89a2a8aedb6d233b74281ccd95231
HugoLepapillon <76493463+HugoLepapillon@users.noreply.github.com>
2023-03-20 16:23:50 +0100

    Homev2

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
+import image1 from '../../../images/Accueil1.png';
+import image2 from '../../../images/Accueil2.png';
+import image3 from '../../../images/Accueil3.png';
 
 function Home() {
   return (
     <div className="Home">
-      <h1>Bienvenue sur Legumz</h1>
-      <p>
-        Legumz est une application de rencontre dédiée aux amateurs de légumes. Inscrivez-vous dès aujourd'hui pour rencontrer des personnes partageant les mêmes intérêts que vous !
-      </p>
+      <div className="header">
+        <h1>Welcum to Legumz</h1>
+        <p>
+          Legumz est une application de rencontre dédiée aux amateurs de
+          légumes. Inscrivez-vous dès aujourd'hui pour rencontrer des personnes
+          partageant les mêmes intérêts que vous !
+        </p>
+      </div>
+      <div className="image-container">
+        <img
+          src={image1}
+          alt="Placeholder"
+          className="image"
+        />
+        <img
+          src={image2}
+          alt="Placeholder"
+          className="image"
+        />
+        <img
+          src={image3}
+          alt="Placeholder"
+          className="image"
+        />
+      </div>
     </div>
   );
 }
 
 export default Home;
-
