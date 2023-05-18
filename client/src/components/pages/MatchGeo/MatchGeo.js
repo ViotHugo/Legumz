1ef65c1a32c6087ff46a7e2edfcb4fb2599c614c
guichouz <guiraudie.gaetan.12@gmail.com>
2023-05-18 18:23:26 +0200

    carte des matchs fonctionnelle.

--- client/src/components/pages/MatchGeo/MatchGeo.js
+++ client/src/components/pages/MatchGeo/MatchGeo.js
@@ -6,6 +6,17 @@ import Header2 from "./../../Header2/Header2";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+import L from "leaflet";
+
+const redIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 const UpdateMapPosition = ({ position }) => {
   const map = useMap();
   map.flyTo(position, 13);
@@ -90,7 +101,7 @@ function MatchGeo() {
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributeurs'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
+          <Marker position={position} icon={redIcon}>
             <Popup>{"Mon emplacement"}</Popup>
           </Marker>
           {
