e1cf884240cb78d6feac8369d40e5531883e54ef
guichouz <guiraudie.gaetan.12@gmail.com>
2023-05-18 12:18:55 +0200

    tchat finito

--- client/src/components/pages/Messages/Messages.js
+++ client/src/components/pages/Messages/Messages.js
@@ -15,12 +15,27 @@ function Messages() {
   const [contactVisible, setContactVisible] = useState({});
   const messagesEndRef = useRef(null);
 
+
+  const [selectedContact, setSelectedContact] = useState(null);
+
+
+  const messageListRef = useRef();
+  const messageContainerRef = useRef();
+
   useEffect(() => {
-    scrollToBottom();
+  scrollToBottom();
   }, [messages]);
 
   function scrollToBottom() {
     messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+
+  function processMessage(message) {
+    let processedMessage = '';
+    for (let i = 0; i < message.length; i += 100) {
+      processedMessage += message.substring(i, i + 100) + '\n';
+    }
+    return processedMessage;
   }
 
   useEffect(() => {
@@ -38,6 +53,7 @@ function Messages() {
         setContacts(response.data);
         if(response.data.length > 0){
           setContactVisible(response.data[0]);
+          setSelectedContact(response.data[0].email);
           const response2 = await axios.post('http://localhost:5000/recupMessages', { email: email });
           setMessages(response2.data);
         }
@@ -54,12 +70,17 @@ function Messages() {
 
   function handleContactClick(contact) {
     setContactVisible(contact);
+    setSelectedContact(contact.email);
     setMessages([...messages]);
   }
 
   function renderContacts() {
     return contacts.map((contact, index) => (
-      <div className="contact" key={index} onClick={() => handleContactClick(contact)}>
+      <div 
+        className={`contactmessage ${contact.email === selectedContact ? 'contact-selected' : ''}`} 
+        key={index} 
+        onClick={() => handleContactClick(contact)}
+      >
         <img
           src={contact.profilePicture}
           alt={contact.firstName}
@@ -67,26 +88,28 @@ function Messages() {
         />
         <span>{contact.firstName}</span>
       </div>
-
     ));
   }
 
   function renderMessages() {
     return messages.map((message) => {
-      if (message.email2 == contactVisible.email && message.email1 == email || message.email1 == contactVisible.email &&  message.email2 == email){
-        let messageClass="";
-        if (message.email1 == email){
-          messageClass = 'message-sent';
-        }
-        else{
-          messageClass = 'message-received';
+      if (
+        (message.email2 === contactVisible.email && message.email1 === email) ||
+        (message.email1 === contactVisible.email && message.email2 === email)
+      ) {
+        let messageClass = "";
+        if (message.email1 === email) {
+          messageClass = "message-sent";
+        } else {
+          messageClass = "message-received";
         }
         return (
           <div className={messageClass}>
             <p>
               <span className="sender">{message.email1}</span>
             </p>
-            <p>{message.message}</p>
+            <pre>{processMessage(message.message)}</pre>
+            {/*<p>{message.message}</p>*/}
           </div>
         );
     }
@@ -109,9 +132,7 @@ function Messages() {
       <div className="messages">
         <div className="messages-content">
               <div className="message-list" >
-                <Draggable axis="y" className="draggable-messages">
                 <div>{renderMessages()}<div ref={messagesEndRef} /></div>
-                </Draggable>
               </div>   
           <form onSubmit={handleSubmit} className="new-message-form">
             <input
